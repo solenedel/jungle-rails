5b7c2db073a7cfe2afde50ff406668f48fce29f3
Solene D <solene98@student.ubc.ca>
2021-09-16 17:36:37 +0000

    create admin/categories controller

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,22 @@
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with :name => ENV['ADMIN_USERNAME'], :password => ENV['ADMIN_PASSWORD']
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'New category created!'
+    else
+      render :new
+    end
+  end
+
+end
