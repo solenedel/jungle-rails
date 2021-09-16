a3824a617e35484a0474dea67f8e518fd6ff0333
Solene D <solene98@student.ubc.ca>
2021-09-16 00:30:57 +0000

    edit display and styles of admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,9 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => ENV['ADMIN_USERNAME'], :password => ENV['ADMIN_PASSWORD']
 
   def show
+    @total_products = Product.count
+    @total_categories = Category.count
   end
+
+
 end
