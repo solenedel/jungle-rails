18e4a4002ef2d3701d552bb9e7e8b86823968f4b
Solene D <solene98@student.ubc.ca>
2021-09-17 18:45:21 +0000

    create sessions controller and add login/logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,11 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
+  post '/login' => 'sessions#create'
+  get '/login' => 'sessions#new'
+  get '/login' => 'sessions#destroy'
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
