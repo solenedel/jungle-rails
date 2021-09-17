a36fcb13a9cdfe0f27f442a24c480af35a7505c2
Solene D <solene98@student.ubc.ca>
2021-09-17 18:37:03 +0000

    rename signin to signup for clarity

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
     resources :categories, except: [:destroy, :edit, :update, :show]
   end
 
-  get '/signin' => 'users#new'
+  get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
