dd217b81595620136856ab78c4946bbc1bcaca72
Solene D <solene98@student.ubc.ca>
2021-09-17 18:33:20 +0000

    add create action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create 
-    
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signin'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation, :name, :email)
+  end
 
 end
