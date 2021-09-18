af2c32749fb6ea236405b13bb2ac831282439b84
Solene D <solene98@student.ubc.ca>
2021-09-18 20:23:28 +0000

    add user authorisation to application controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
   end
 
   def destroy
   end
-  
+
 end
