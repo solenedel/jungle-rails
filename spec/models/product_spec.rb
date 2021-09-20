95e73869edaa3098e5faafb0171a9a6b4bae1aa8
Solene D <solene98@student.ubc.ca>
2021-09-20 21:56:52 +0000

    add name field test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,10 +6,23 @@ RSpec.describe Product, type: :model do
 
     # when all fields are present, product saves successfully
     it 'saves successfully when all required fields are provided' do
-      @category = Category.new(name: 'test')
+      @category = Category.new(name: 'test category')
       @product = @category.products.new(name: 'test product', price: 40, quantity: 3)
       @product.save!
     end
+
+     # when name not provided
+     it 'returns an error when the name field is not provided' do
+      @category = Category.new(name: 'test category')
+      @product = @category.products.new( price: 40, quantity: 3)
+      @product.save
+      puts @product.errors.full_messages
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+
+
+    
+
 
   end
 
