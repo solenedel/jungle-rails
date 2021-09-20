275d39729fc382184e18aaf262fe3608f0c88d23
Solene D <solene98@student.ubc.ca>
2021-09-20 22:09:01 +0000

    finish products tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -20,6 +20,31 @@ RSpec.describe Product, type: :model do
       expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
+     # when price not provided
+     it 'returns an error when the price field is not provided' do
+      @category = Category.new(name: 'test category')
+      @product = @category.products.new(name: 'test product', quantity: 3)
+      @product.save
+      puts @product.errors.full_messages
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+    
+    # when quantity not provided
+    it 'returns an error when the quantity field is not provided' do
+      @category = Category.new(name: 'test category')
+      @product = @category.products.new(name: 'test product', price: 40)
+      @product.save
+      puts @product.errors.full_messages
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    # when category not provided
+    it 'returns an error when the category field is not provided' do
+      @product = Product.new(name: 'test product', price: 40, quantity: 3)
+      @product.save
+      puts @product.errors.full_messages
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
 
     
 
