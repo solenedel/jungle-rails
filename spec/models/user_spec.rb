ddf46365a730cd2356d87648d43377d00ac43e8f
Solene D <solene98@student.ubc.ca>
2021-09-21 21:25:27 +0000

    add tests for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,8 +10,29 @@ RSpec.describe User, type: :model do
       @user.save
       expect(@user.errors.full_messages).to include("Passwords don't match")
     end
-  end
 
-  #
+    # first name must be provided
+    it 'returns an error if first name is not provided'
+      @user = User.new(last_name: 'Pi', email: 'applepi@mail.com', password: 'badpassword', password_confirmation: 'badpassword')
+      @user.save
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+    end
+
+    # last name must be provided
+    it 'returns an error if last name is not provided'
+      @user = User.new(first_name: 'Apple', email: 'applepi@mail.com', password: 'badpassword', password_confirmation: 'badpassword')
+      @user.save
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
+    end
+
+     # email must be provided
+     it 'returns an error if email is not provided'
+     @user = User.new(first_name: 'Apple', last_name: 'Pi', password: 'badpassword', password_confirmation: 'badpassword')
+     @user.save
+     expect(@user.errors.full_messages).to include("Email can't be blank")
+   end
+
+
+  end
 
 end
