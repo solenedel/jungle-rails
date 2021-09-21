bc2dc76d8bcb59c2f43e1433284db9a7e25668ef
Solene D <solene98@student.ubc.ca>
2021-09-21 21:17:29 +0000

    add first Users test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'Validations' do
+
+    # check that password and confirmation match
+    it 'returns an error if password and password confirmation do not match'
+      @user = User.new(first_name: 'Apple', last_name: 'Pi', email: 'applepi@mail.com', password: 'badpassword', password_confirmation: 'worsepassword')
+      @user.save
+      expect(@user.errors.full_messages).to include("Passwords don't match")
+    end
+  end
+
+  #
+
 end
