08114ea86591a8735594006ecd4fa27703f1b1ec
Solene D <solene98@student.ubc.ca>
2021-09-23 17:44:15 +0000

    test user navigation

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+
+RSpec.feature "user can navigate from the home page to the product detail page by clicking on a product", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see the product details" do
+    # ACT
+    visit root_path
+    first('.product').click_on('Details')
+    # DEBUG
+    save_screenshot
+
+    # VERIFY
+    expect(page).to have_css 'article.product-detail'
+
+    save_screenshot
+  end
+end
